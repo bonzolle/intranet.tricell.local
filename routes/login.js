248950926f9871a810c6436a1ab697a092557f7f
bonzolle <21311380+bonzolle@users.noreply.github.com>
2026-02-26 19:59:29 +0200

    added login page

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const readHTML = require('../readHTML.js');
+const Database = require('better-sqlite3');
+const path = require('path');
 
-router.get('/', (request, response) =>
-{
-    response.send('Login');
+const dbPath = path.join(__dirname, '..', 'users.db');
+const db = new Database(dbPath);
+
+
+router.use(express.static('./public'));
+var htmlHead = readHTML('./head.html');
+var htmlHeader = readHTML('./header.html');
+var htmlMenu = readHTML('./menu_back.html');
+var htmlInfoStart = readHTML('./infoStart.html');
+var htmlInfoStop = readHTML('./infoStop.html');
+var htmlFooter = readHTML('./footer.html');
+var htmlBottom = readHTML('./bottom.html');
+
+router.get('/', (request, response) => {
+
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write(htmlHead);
+    response.write(htmlHeader);
+    response.write(htmlMenu);
+    response.write(htmlInfoStart);
+
+
+    response.write(htmlInfoStop);
+    response.write(htmlFooter);
+    response.write(htmlBottom);
+    response.end();
+
 });
 
 module.exports = router;
